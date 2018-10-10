f1e2ec0c23f9ad60bb13d0af76588a882f3330f4
Angus Fisk <angus.fisk@ndcn.ox.ac.uk>
2018-10-10 16:00:53 +0100

    Updates with commas to work pip

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="actigraphy_analysis"
+    name="actigraphy_analysis",
     version="0.0.1",
-    author="Angus Fisk
-    author_email="angus_fisk@hotmail.com"
-    description="Scripts for analysis of actigraphy data"
+    author="Angus Fisk", 
+    author_email="angus_fisk@hotmail.com",
+    description="Scripts for analysis of actigraphy data",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/A-Fisk/actigraphy_analysis"
+    url="https://github.com/A-Fisk/actigraphy_analysis",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3.0"
+        "License :: OSI Approved :: GNU General Public License v3.0",
         "Operating System :: OS Independent",
     ],
 )
