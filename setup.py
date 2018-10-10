7bdca885fcaea5407614174c40747f42034b2e2e
Angus Fisk <angus.fisk@ndcn.ox.ac.uk>
2018-10-10 15:54:28 +0100

    Sorting files for travis and environment
    Tries to get pip install to work

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example_pkg",
+    name="actigraphy_analysis
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="Angus Fisk
+    author_email="angus_fisk@hotmail.com"
+    description="Scripts for analysis of actigraphy data"
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/A-Fisk/actigraphy_analysis"
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3.0"
         "Operating System :: OS Independent",
     ],
 )
