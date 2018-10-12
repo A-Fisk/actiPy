e4f31a32d0d24a5aa6c5a5e835def9e7879086de
Angus Fisk <angus.fisk@ndcn.ox.ac.uk>
2018-10-12 23:29:02 +0100

    updates setup py package list

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/A-Fisk/actigraphy_analysis",
-    packages=setuptools.find_packages(),
+    packages=['actigraphy_analysis']
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3.0",
