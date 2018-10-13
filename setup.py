7cc8a413b16f72c4cca7b5ef9e3a8b010eed7cdb
Angus Fisk <angus.fisk@ndcn.ox.ac.uk>
2018-10-13 08:22:41 +0100

    Updates setup.py
    import working now.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="actigraphyanalysis",
+setup(    
+    name="actigraphy_analysis",
     version="0.0.1",
     author="Angus Fisk", 
     author_email="angus_fisk@hotmail.com",
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/A-Fisk/actigraphy_analysis",
-    packages=['actigraphy_analysis'], 
+    packages=find_packages(), 
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3.0",
