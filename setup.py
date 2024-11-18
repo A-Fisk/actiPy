59338630f07d1ba450fdd20bf29eec4a2c1601ab
Angus Fisk <angus.fisk@bnc.ox.ac.uk>
2024-11-18 21:29:04 +1100

    V.0.1.2

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setup(    
     name="actiPy",
-    version="0.1.1",
+    version="0.1.2",
     author="Angus Fisk", 
     author_email="angus_fisk@hotmail.com",
     description="Scripts for analysis of actigraphy data",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/A-Fisk/actigraphy_analysis",
+    url="https://github.com/aa-fisk/actigraphy_analysis",
     packages=find_packages(), 
     classifiers=[
         "Programming Language :: Python :: 3",
